7a888548c396b21495973305006bd3bc827b78a9
jbelenob <jbelenob@unal.edu.co>
2022-05-04 10:36:27 -0500

    Se implementó la funcion fill

--- initial.cpp
+++ initial.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <random>
+
+void fill_matrix(std::vector<long> & data, int seed);
+void print_matrix(const std::vector <long> & data);
 
 int main(int argc, char** argv)
 {
+  //size debe ser un numero cuadrado.
+  long size = std::atoi(argv[1]);
 
-  return ;,
+  const int SEED = std::atoi(argv[2]);
+
+  std::vector <long> m1(size*size, 0);
+
+  return 0;
+}
+
+void fill_matrix(std::vector<long> & data, int seed)
+{
+  const long size = std::sqrt(data.size());
+
+  std::mt19937 gen(seed);
+
+  std::uniform_int_distribution<> dis(0, 1);
+
+for(long ii = 0; ii < size; ii++)
+  {
+    for(long jj = 0; jj < size; jj++)
+    {
+      data[ii*size + jj] = dis(gen);
+    }
+  } 
 }
