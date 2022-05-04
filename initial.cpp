2f2a620952e04d8a144c0d76c5778320815ea671
jbelenob <jbelenob@unal.edu.co>
2022-05-04 17:43:06 -0500

    Ahora nonclassified_cluster retorna la posición del cluster no clasificado, si no hay retorna un 0

--- initial.cpp
+++ initial.cpp
@@ -13,7 +13,11 @@
 
 void fill_matrix(std::vector<long> & data, int seed, double p);
 void print_matrix(const std::vector <long> & data);
-bool find_nonclassified_cluster(std::vector <long> & data);
+
+/*Si hay algun cluster sin clasificar retorna la posición el vector
+ * del primer elemento al clasificar de ese cluster. Si no hay entonces retorna .1.
+ */
+long find_nonclassified_cluster(std::vector <long> & data);
 //void classify_clusters(std::vector <long> & data);
 
 int main(int argc, char** argv)
@@ -25,11 +29,11 @@ int main(int argc, char** argv)
 
   std::vector <long> m1(size*size, 0);
 
-  //std::cout << find_nonclassified_cluster(m1) << '\n';
+  std::cout << find_nonclassified_cluster(m1) << '\n';
 
   fill_matrix(m1, SEED, probability);
 
-  //std::cout << find_nonclassified_cluster(m1) << '\n';
+  std::cout << find_nonclassified_cluster(m1) << '\n';
 
   print_matrix(m1);
 
@@ -68,7 +72,7 @@ for(long ii = 0; ii < size; ii++)
   } 
 }
 
-bool find_nonclassified_cluster(std::vector <long> & data)
+long find_nonclassified_cluster(std::vector <long> & data)
 {
   const long total_elements = data.size();
   
@@ -76,10 +80,10 @@ bool find_nonclassified_cluster(std::vector <long> & data)
     {
       if(data[ii] == 1)
 	{
-	  return true;
+	  return ii;
 	}
     }
-  return false;
+  return -1;
 }
 
 /*void classify_clusters(std::vector <long> & data)
